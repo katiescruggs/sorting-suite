d7a817cc83e7322293f31608c0309950a2e46101
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-19 15:03:08 -0600

    add optimizations for bubble and insertion sorts

--- insertionSort.js
+++ insertionSort.js
@@ -1,12 +1,10 @@
 module.exports = function insertionSort(arr) {
   for(let i = 1; i < arr.length; i++) {
-    if(arr[i] < arr[i-1]) {
-      for(let j = i; j > 0; j--) {
-        if(arr[j] < arr[j-1]) {
+      for(let j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+        // if(arr[j] < arr[j-1]) {
           [ arr[j], arr[j-1] ] = [ arr[j-1], arr[j] ];
-        }
+        // }
       }
-    }
   }
   return arr;
 };
