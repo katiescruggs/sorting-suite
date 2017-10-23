bd23a93d112cf2272239c86ed419fe55340f3a67
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-23 13:41:00 -0600

    create index to export files for NPM module

---
+++ index.js
@@ -0,0 +1,11 @@
+const bubbleSort = require('./bubbleSort.js');
+const insertionSort = require('./insertionSort.js');
+const mergeSort = require('./mergeSort.js');
+const quickSort = require('./quickSort.js');
+
+module.exports = {
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort
+};
